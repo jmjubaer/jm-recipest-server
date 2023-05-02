d4385a148fb7cccd5a8a535da93d4a4a6adbe11e
jmjubaer <jmjubaer3927@gmail.com>
2023-05-02 15:44:46 +0600

    send recipes data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = 5000;
 const chefData = require('./data/ChefInfo.json');
+const recipeData = require('./data/recipe.json');
+
 
 app.use(cors());
 
@@ -12,6 +14,10 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
     res.send(chefData)
+  })
+
+app.get('/recipes', (req, res) => {
+    res.send(recipeData)
   })
 
 app.listen(port, () => {
