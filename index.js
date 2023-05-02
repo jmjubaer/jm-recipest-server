ed71f735373bb9b8e7c26c8db96b7ba89cbd3175
jmjubaer <jmjubaer3927@gmail.com>
2023-05-02 10:47:11 +0600

    server is  ready

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running at http://localhost:5000')
+  })
+  
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
