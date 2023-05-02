5063cc15d1e5f97e76f026b161e7bf54df1f7425
jmjubaer <jmjubaer3927@gmail.com>
2023-05-02 11:14:23 +0600

    Add chef data in server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
+const chefData = require('./data/ChefInfo.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Server is running at http://localhost:5000')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefData)
   })
-  
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
   })
